946ea1f43971f81aa883eafcf828ef3eadd8c428
lawsonjerry <williambookman@pursuit.org>
2024-04-02 19:36:44 -0400

    added some basic copy to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-
-
-    </div>
+    <footer className="footer">
+      <div className="container">
+        <p>© {new Date().getFullYear()} Foodbriiite. All rights reserved.</p>
+        <p>Contact us at: contact@foodbriiite.com</p>
+        {/* <p>Follow us on social media:</p>
+        <ul className="social-media-links">
+          <li><a href="https://www.facebook.com/yourcompany" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+          <li><a href="https://www.twitter.com/yourcompany" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+          <li><a href="https://www.linkedin.com/company/yourcompany" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+        </ul> */}
+      </div>
+    </footer>
   )
 }
 
