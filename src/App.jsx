7de71ec1c650d799126dec404705fe77164dca50
lawsonjerry <williambookman@pursuit.org>
2024-04-02 21:33:50 -0400

    creatveEventForm component created with setTitle, handlesubmit, and handleInputChange testing out title only

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Footer from './components/Footer.jsx'
+import CreateEventForm from './components/CreateEventForm.jsx'
 import './App.css'
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-
+<CreateEventForm />
         <Footer />
         </div>
         
---
+++ src/components/CreateEventForm.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+
+const CreateEventForm = () => {
+  const [title, setTitle] = useState('');
+
+  const handleInputChange = (event) => {
+    const { name, value, type, files } = event.target;
+    switch (type) {
+      case 'text':
+        setName(value);
+        break;
+      case 'date':
+        setDate(new Date(value));
+        break;
+      case 'time':
+        setTime(value);
+        break;
+      case 'file':
+        setImageFile(files[0]);
+        break;
+      default:
+        if (event.target.checked) {
+          setFoodDonations([...foodDonations, value]);
+        } else {
+          setFoodDonations(foodDonations.filter((item) => item !== value));
+        }
+    }
+   
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Validation logic here (e.g., check for required fields)
+
+    // Submit form data (title, date, time, location, description, foodDonations, imageFile)
+    console.log('Submitted event data:', {
+      title,
+      date,
+      time,
+      location,
+      description,
+      foodDonations,
+      imageFile,
+    });
+    // Reset form after successful submission (optional)
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Create Food Donation Event</h2>
+      <div className="form-group">
+        <label htmlFor="title">Event Title:</label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          value={title}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      </form>
+  )
+}
+
+export default CreateEventForm
